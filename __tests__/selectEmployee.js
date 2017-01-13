bafaff0d32dbce573d3fdc9fd3ba0d6193e63c64
dibpuelma <edib@uc.cl>
2017-01-13 13:37:59 -0300

    Render test pass

--- __tests__/selectEmployee.js
+++ __tests__/selectEmployee.js
@@ -1,12 +1,17 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.android.js';
+import SelectEmployee from '../app/routes/SelectEmployee/selectEmployee'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+storeData = {
+  data:"encuesta1",
+  type:"QR"
+}
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
-  );
+    <SelectEmployee codeData={storeData}/>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
