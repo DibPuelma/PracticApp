e1fd77206f5a0f0f542103ecb9a91df6186090fd
dibpuelma <edib@uc.cl>
2017-02-22 16:30:19 -0300

    Commented notifications because library version changed

--- android/app/src/main/java/com/practica/MainApplication.java
+++ android/app/src/main/java/com/practica/MainApplication.java
@@ -18,13 +18,12 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-import com.geektime.reactnativeonesignal.ReactNativeOneSignalPackage;
-
-public class MainApplication extends Application implements ReactApplication {
 import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.appevents.AppEventsLogger;
+
+public class MainApplication extends Application implements ReactApplication {
+
 
   private static CallbackManager mCallbackManager =
     CallbackManager.Factory.create();
